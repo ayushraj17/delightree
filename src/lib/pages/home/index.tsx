1314d0b54886ad4004510234860ec967ae23ba81
ayushraj17 <meayushraj17@gmail.com>
2024-03-24 18:22:40 +0530

    init: repo

---
+++ src/lib/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Grid } from '@chakra-ui/react';
+
+import HookForm from './components/Form';
+
+const Home = () => {
+  return (
+    <Grid gap={4}>
+      <HookForm />
+    </Grid>
+  );
+};
+export default Home;
